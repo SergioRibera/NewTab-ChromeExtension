b32ec31f717f492c35a68cf5171461779d0a414d
Sergio Alejandro Ribera Costa <sergioalejandroriberacosta@gmail.com>
2021-01-08 01:46:33 -0400

    Fix Some Bugs

--- tab.js
+++ tab.js
@@ -95,136 +95,133 @@ function ChangeProp (name, value){
         ResetSettings();
     };
     chrome.storage.local.get('conf', result => {
-        if(result.conf){
+        if(result.conf)
             settings = result.conf;
-            console.log("exists");
-        }else{
-            console.log("not exists");
+        else
             SaveData();
-        }
-        console.log(result.conf);
-        console.log(settings);
-    });
-    if(settings.BG.useNASA == true)
-        UseNASA();
-    else if(settings.BG.useColor)
-        ChangeProp('--background-general', settings.BG.color);
-    ChangeProp('--color-clock', settings.CLOCK.color)
-    ChangeProp('--font-clock', settings.CLOCK.font);
-    ChangeProp('--font-size-clock', settings.CLOCK.size);
-
-    let inBlur = document.getElementById('blur');
-    let inUseColor = document.getElementById('useColor');
-    let inUseNASA = document.getElementById('useNASA');
-    let inUseCustomIMG = document.getElementById('useCustomIMG');
-    let inBgColor = document.getElementById('bg-color');
-    let inBgCimage = document.getElementById('bg-cimage');
-
-    let inCFont = document.getElementById('clock-font');
-    let inCColor = document.getElementById('clock-color');
-    let inCSize = document.getElementById('clock-size');
-    let inCPosH = document.getElementById('clock-h');
-    let inCPosV = document.getElementById('clock-v');
-    inBlur.checked = settings.BG.blur;
-    inUseColor.checked = settings.BG.useColor;
-    inUseNASA.checked = settings.BG.useNASA;
-    inUseCustomIMG.checked = settings.BG.useCustomIMG;
-    inBgColor.value = settings.BG.color;
-    inBgCimage.value = settings.BG.customIMG;
-    // Clock Settings
-    inCFont.value = settings.CLOCK.font;
-    inCColor.value = settings.CLOCK.color;
-    inCSize.value = settings.CLOCK.size;
-    inCPosV.value = settings.CLOCK.pos.v;
-    inCPosH.value = settings.CLOCK.pos.h;
-
-    inBlur.addEventListener('change', e => {
-        settings.BG.blur = e.target.checked;
-        if(settings.BG.blur)
-            ChangeProp('--background-effect', 'blur(3px)');
-        else
-            ChangeProp('--background-effect', 'none');
-        SaveData();
-    });
-    inUseColor.addEventListener('change', e => {
-        settings.BG.useColor = e.target.checked;
-        if(settings.BG.useColor)
-            ChangeProp('--background-general', settings.BG.color);
-        else
-            if(!settings.BG.useNASA)
-                ChangeProp('--background-general', '');
-        else if(settings.BG.customIMG)
-            ChangeProp('--background-general', settings.BG.customIMG);
-        else
+        if(settings.BG.useNASA == true)
             UseNASA();
-        SaveData();
-    });
-    inUseNASA.addEventListener('change', e => {
-        settings.BG.useNASA = e.target.checked;
-        if(settings.BG.useColor)
+        else if(settings.BG.useColor)
             ChangeProp('--background-general', settings.BG.color);
-        else
-            if(!settings.BG.useNASA)
-                ChangeProp('--background-general', '');
-        else if(settings.BG.customIMG)
-            ChangeProp('--background-general', settings.BG.customIMG);
-        else
-            UseNASA();
-        SaveData();
-    });
-    inUseCustomIMG.addEventListener('change', e => {
-        settings.BG.useCustomIMG = e.target.checked;
-        if(settings.BG.useColor)
-            ChangeProp('--background-general', settings.BG.color);
-        else
-            if(!settings.BG.useNASA)
-                ChangeProp('--background-general', '');
-        else if(settings.BG.customIMG)
-            ChangeProp('--background-general', settings.BG.customIMG);
-        else
-            UseNASA();
-        SaveData();
-    });
-
-    inBgColor.addEventListener('change', e => {
-        settings.BG.color = e.target.value;
-        settings.BG.useColor = true;
-        inUseColor.checked = true;
-        ChangeProp('--background-general', settings.BG.color);
-        SaveData();
-    });
-    inBgCimage.addEventListener('change', e => {
-        settings.BG.customIMG = e.target.value;
-        settings.BG.useCustomIMG = true;
-        inBgCimage.checked = true;
-        ChangeProp('--background-general', settings.BG.customIMG);
-        SaveData();
-    });
-    inCFont.addEventListener('change', e => {
-        settings.CLOCK.font = e.target.value;
+        ChangeProp('--color-clock', settings.CLOCK.color)
         ChangeProp('--font-clock', settings.CLOCK.font);
-        SaveData();
-    });
-    inCColor.addEventListener('change', e => {
-        settings.CLOCK.color = e.target.value;
-        ChangeProp('--color-clock', settings.CLOCK.color);
-        SaveData();
-    });
-
-    inCSize.addEventListener('change', e => {
-        settings.CLOCK.size = e.target.value;
         ChangeProp('--font-size-clock', settings.CLOCK.size);
-        SaveData();
-    });
-    inCPosV.addEventListener('change', e => {
-        settings.CLOCK.pos.v = e.target.value;
         ChangeProp('--pos-clock-vertical', settings.CLOCK.pos.v);
-        SaveData();
-    });
-    inCPosH.addEventListener('change', e => {
-        settings.CLOCK.pos.h = e.target.value;
         ChangeProp('--pos-clock-horizontal', settings.CLOCK.pos.h);
-        SaveData();
-    });
 
+
+        let inBlur = document.getElementById('blur');
+        let inUseColor = document.getElementById('useColor');
+        let inUseNASA = document.getElementById('useNASA');
+        let inUseCustomIMG = document.getElementById('useCustomIMG');
+        let inBgColor = document.getElementById('bg-color');
+        let inBgCimage = document.getElementById('bg-cimage');
+
+        let inCFont = document.getElementById('clock-font');
+        let inCColor = document.getElementById('clock-color');
+        let inCSize = document.getElementById('clock-size');
+        let inCPosH = document.getElementById('clock-h');
+        let inCPosV = document.getElementById('clock-v');
+        inBlur.checked = settings.BG.blur;
+        inUseColor.checked = settings.BG.useColor;
+        inUseNASA.checked = settings.BG.useNASA;
+        inUseCustomIMG.checked = settings.BG.useCustomIMG;
+        inBgColor.value = settings.BG.color;
+        inBgCimage.value = settings.BG.customIMG;
+        // Clock Settings
+        inCFont.value = settings.CLOCK.font;
+        inCColor.value = settings.CLOCK.color;
+        inCSize.value = settings.CLOCK.size;
+        inCPosV.value = settings.CLOCK.pos.v;
+        inCPosH.value = settings.CLOCK.pos.h;
+
+        inBlur.addEventListener('change', e => {
+            settings.BG.blur = e.target.checked;
+            if(settings.BG.blur)
+                ChangeProp('--background-effect', 'blur(3px)');
+            else
+                ChangeProp('--background-effect', 'none');
+            SaveData();
+        });
+        inUseColor.addEventListener('change', e => {
+            settings.BG.useColor = e.target.checked;
+            if(settings.BG.useColor)
+                ChangeProp('--background-general', settings.BG.color);
+            else
+                if(!settings.BG.useNASA)
+                    ChangeProp('--background-general', '');
+            else if(settings.BG.customIMG)
+                ChangeProp('--background-general', settings.BG.customIMG);
+            else
+                UseNASA();
+            SaveData();
+        });
+        inUseNASA.addEventListener('change', e => {
+            settings.BG.useNASA = e.target.checked;
+            if(settings.BG.useColor)
+                ChangeProp('--background-general', settings.BG.color);
+            else
+                if(!settings.BG.useNASA)
+                    ChangeProp('--background-general', '');
+            else if(settings.BG.customIMG)
+                ChangeProp('--background-general', settings.BG.customIMG);
+            else
+                UseNASA();
+            SaveData();
+        });
+        inUseCustomIMG.addEventListener('change', e => {
+            settings.BG.useCustomIMG = e.target.checked;
+            if(settings.BG.useColor)
+                ChangeProp('--background-general', settings.BG.color);
+            else
+                if(!settings.BG.useNASA)
+                    ChangeProp('--background-general', '');
+            else if(settings.BG.customIMG)
+                ChangeProp('--background-general', settings.BG.customIMG);
+            else
+                UseNASA();
+            SaveData();
+        });
+
+        inBgColor.addEventListener('change', e => {
+            settings.BG.color = e.target.value;
+            settings.BG.useColor = true;
+            inUseColor.checked = true;
+            ChangeProp('--background-general', settings.BG.color);
+            SaveData();
+        });
+        inBgCimage.addEventListener('change', e => {
+            settings.BG.customIMG = e.target.value;
+            settings.BG.useCustomIMG = true;
+            inBgCimage.checked = true;
+            ChangeProp('--background-general', settings.BG.customIMG);
+            SaveData();
+        });
+        inCFont.addEventListener('change', e => {
+            settings.CLOCK.font = e.target.value;
+            ChangeProp('--font-clock', settings.CLOCK.font);
+            SaveData();
+        });
+        inCColor.addEventListener('change', e => {
+            settings.CLOCK.color = e.target.value;
+            ChangeProp('--color-clock', settings.CLOCK.color);
+            SaveData();
+        });
+
+        inCSize.addEventListener('change', e => {
+            settings.CLOCK.size = e.target.value;
+            ChangeProp('--font-size-clock', settings.CLOCK.size);
+            SaveData();
+        });
+        inCPosV.addEventListener('change', e => {
+            settings.CLOCK.pos.v = e.target.value;
+            ChangeProp('--pos-clock-vertical', settings.CLOCK.pos.v);
+            SaveData();
+        });
+        inCPosH.addEventListener('change', e => {
+            settings.CLOCK.pos.h = e.target.value;
+            ChangeProp('--pos-clock-horizontal', settings.CLOCK.pos.h);
+            SaveData();
+        });
+    });
 }());
